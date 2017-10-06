015d99631258c9a9cc639d1486187718db124a41
royernst <roy.e31@hotmail.com>
2017-10-06 14:57:38 -0400

    Part 4 and stretch tast complete

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -15,51 +15,57 @@ namespace LuckyNumbers
             int[] guessedNums = new int[6];
             decimal numCorrect = 0;
             decimal jackpot = 10312494.00m;
-
+            string answer;
 
             Console.WriteLine("Booting LuckyNumbers.exe...");
-            Console.WriteLine("\nIf you are able to guess all 6 numbers, you will win the jackpot!!");
-            Console.WriteLine("Current jackpot: $10,312,494");
-            Console.WriteLine("You can still win great prizes if you guess between 1 and 5 numbers correctly!");
-            Console.WriteLine("\nPlease enter 2 numbers that you would like to set as your range.");
-            Console.Write("Low range: ");
-            lowRange = int.Parse(Console.ReadLine());
-            Console.Write("High range: ");
-            highRange = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            for (int i = 0; i < 6; i++)
+
+            do
             {
-                Console.Write("Please enter a guess between your number ranges: ");
-                guessedNums[i] = int.Parse(Console.ReadLine());
-                while (guessedNums[i] < lowRange || guessedNums[i] > highRange)
+                Console.WriteLine("\nIf you are able to guess all 6 numbers, you will win the jackpot!!");
+                Console.WriteLine("Current jackpot: $10,312,494");
+                Console.WriteLine("You can still win great prizes if you guess between 1 and 5 numbers correctly!");
+                Console.WriteLine("\nPlease enter 2 numbers that you would like to set as your range.");
+                Console.Write("Low range: ");
+                lowRange = int.Parse(Console.ReadLine());
+                Console.Write("High range: ");
+                highRange = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                for (int i = 0; i < 6; i++)
                 {
-                    Console.WriteLine("That entry is incorrect");
-                    Console.Write("Please enter a valid number: ");
+                    Console.Write("Please enter a guess between your number ranges: ");
                     guessedNums[i] = int.Parse(Console.ReadLine());
+                    while (guessedNums[i] < lowRange || guessedNums[i] > highRange)
+                    {
+                        Console.WriteLine("That entry is incorrect");
+                        Console.Write("Please enter a valid number: ");
+                        guessedNums[i] = int.Parse(Console.ReadLine());
+
+                    }
 
                 }
+                int[] randArray = RandNum(lowRange, highRange, guessedNums.Length);
 
-            }
-            int[] randArray = RandNum(lowRange, highRange, guessedNums.Length);
-
-            Console.WriteLine("Here are today's lucky numbers:");
-            for (int i = 0; i < randArray.Length; i++)
-            {
-                Console.WriteLine(randArray[i]);
-            }
-
-            for (int i = 0; i < randArray.Length; i++)
-            {
-                if (randArray.Contains(guessedNums[i]))
+                Console.WriteLine("Here are today's lucky numbers:");
+                for (int i = 0; i < randArray.Length; i++)
                 {
-                    numCorrect++;
+                    Console.WriteLine(randArray[i]);
                 }
-            }
 
-            Console.WriteLine("You guessed {0} numbers correctly!", numCorrect);
-            Console.WriteLine("You won $" + Math.Round(Winnings(jackpot, numCorrect), 2));
+                for (int i = 0; i < randArray.Length; i++)
+                {
+                    if (randArray.Contains(guessedNums[i]))
+                    {
+                        numCorrect++;
+                    }
+                }
 
+                Console.WriteLine("You guessed {0} numbers correctly!", numCorrect);
+                Console.WriteLine("You won $" + Math.Round(Winnings(jackpot, numCorrect), 2));
+                Console.Write("Would you like to play again? (YES/NO)");
+                answer = Console.ReadLine().Trim().ToLower();
 
+            } while (answer == "yes");
+            Console.WriteLine("Thanks for playing!");
         }
 
         public static int[] RandNum(int lowValue, int highValue, int arraySize)
@@ -68,12 +74,13 @@ namespace LuckyNumbers
             int[] array = new int[arraySize];
             for (int i = 0; i < arraySize; i++)
             {
-                array[i] = randGen.Next(lowValue, (highValue + 1));
-                //while (array.Contains(array[i]) && i !=0)
-                //{
-                //    array[i] = randGen.Next(lowValue, (highValue + 1));
-                //}
-
+                int randNum = randGen.Next(lowValue, (highValue + 1));
+                
+                while (array.Contains(randNum) && i != 0)
+                {
+                    randNum = randGen.Next(lowValue, (highValue + 1));
+                }
+                array[i] = randNum;
             }
             return array;
 
@@ -85,7 +92,7 @@ namespace LuckyNumbers
             decimal totalWinnings = jackpot * (correct / 6m);
             totalWinnings = Math.Round(totalWinnings, 2);
             return totalWinnings;
-            
+
         }
     }
 }
