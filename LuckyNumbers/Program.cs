c4b6a334df2a083c9c36e0027faeddee490433f6
royernst <roy.e31@hotmail.com>
2017-10-06 13:57:51 -0400

    Finished part 3

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -13,7 +13,9 @@ namespace LuckyNumbers
             int highRange = 0;
             int lowRange = 0;
             int[] guessedNums = new int[6];
-            
+            decimal numCorrect = 0;
+            decimal jackpot = 10312494.00m;
+
 
             Console.WriteLine("Booting LuckyNumbers.exe...");
             Console.WriteLine("\nIf you are able to guess all 6 numbers, you will win the jackpot!!");
@@ -35,8 +37,8 @@ namespace LuckyNumbers
                     Console.Write("Please enter a valid number: ");
                     guessedNums[i] = int.Parse(Console.ReadLine());
 
-                } 
-                
+                }
+
             }
             int[] randArray = RandNum(lowRange, highRange, guessedNums.Length);
 
@@ -45,6 +47,19 @@ namespace LuckyNumbers
             {
                 Console.WriteLine(randArray[i]);
             }
+
+            for (int i = 0; i < randArray.Length; i++)
+            {
+                if (randArray.Contains(guessedNums[i]))
+                {
+                    numCorrect++;
+                }
+            }
+
+            Console.WriteLine("You guessed {0} numbers correctly!", numCorrect);
+            Console.WriteLine("You won $" + Math.Round(Winnings(jackpot, numCorrect), 2));
+
+
         }
 
         public static int[] RandNum(int lowValue, int highValue, int arraySize)
@@ -54,15 +69,23 @@ namespace LuckyNumbers
             for (int i = 0; i < arraySize; i++)
             {
                 array[i] = randGen.Next(lowValue, (highValue + 1));
-                //while (array.Contains (array[i]) && i != 0)
+                //while (array.Contains(array[i]) && i !=0)
                 //{
-                //    array[i] = randGen.Next(lowValue, (highValue + 1)); 
+                //    array[i] = randGen.Next(lowValue, (highValue + 1));
                 //}
-                           
+
             }
             return array;
-            
 
+
+        }
+
+        public static decimal Winnings(decimal jackpot, decimal correct)
+        {
+            decimal totalWinnings = jackpot * (correct / 6m);
+            totalWinnings = Math.Round(totalWinnings, 2);
+            return totalWinnings;
+            
         }
     }
 }
